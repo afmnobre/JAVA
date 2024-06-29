11ae15d6bc7e170d6e10a22f4d735b2ea2d6186f
Seu Nome <lucas_guitar1987@hotmail.com>
2024-06-29 14:09:30 -0300

    Corrigindo Java no Arch

---
+++ Java/Curso_Udemy/Exercicio1/Exercicio1/src/Exemplo2.java
@@ -0,0 +1,13 @@
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+        resultado = (double) a / b;
+        System.out.println("Transformando 2 Insteiros em Double FLUTUANTE:");
+        System.out.println(resultado);
+
+    }
+}

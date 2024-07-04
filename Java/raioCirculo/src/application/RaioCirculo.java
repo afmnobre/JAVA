6d6d0c729145797020837d74795ae4d541324409
afmnobre <lucas_guitar1987@hotmail.com>
2024-07-04 10:43:09 -0300

    Salvando mudanças antes de reescrever o historico

--- Java/raioCirculo/src/application/RaioCirculo.java
+++ Java/raioCirculo/src/application/RaioCirculo.java
@@ -9,17 +9,21 @@ public class RaioCirculo {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
+		//Calculator calc = new Calculator(); // INSTANCIANDO
 		
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = calc.circumference(radius);
-		double v = calc.volume(radius);
+		//double c = calc.circumference(radius); //INSTANCIANDO
+		//double v = calc.volume(radius); //INSTANCIANDO
+		double c = Calculator.circumference(radius);
+		double v = Calculator.volume(radius);
+				
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Value: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI );
+		//System.out.printf("PI value: %.2f%n", calc.PI ); //INSTANCIANDO
+		System.out.printf("PI value: %.2f%n", Calculator.PI );
 		
 		sc.close();
 	}

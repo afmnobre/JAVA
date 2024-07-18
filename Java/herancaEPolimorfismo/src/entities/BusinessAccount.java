82b9c0265fd3615e65b92400ba021e31d48657e6
afmnobre <lucas_guitar1987@hotmail.com>
2024-07-18 16:18:53 -0300

    Herança e Polimorfismo

---
+++ Java/herancaEPolimorfismo/src/entities/BusinessAccount.java
@@ -0,0 +1,31 @@
+package entities;
+
+import entitites.Account;
+
+public class BusinessAccount extends Account {
+
+	private Double loanLimit;
+	
+	public BusinessAccount() {
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void Loan(double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0; // BALANCE esta na classe principal
+		}
+	}
+
+}

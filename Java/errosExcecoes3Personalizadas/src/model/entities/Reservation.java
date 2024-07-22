676c3757244f56ad8ba6f96d6dffef0ca043435c
afmnobre <lucas_guitar1987@hotmail.com>
2024-07-22 01:15:36 -0300

    Exercicio para tratamento de erros

---
+++ Java/errosExcecoes3Personalizadas/src/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date 	checkIn;
+	private Date 	checkOut;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+		super();
+	}
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		//Transforma os dias em MILISEGUNDOS e DEPOIS faz a conta.
+		long diff = checkOut.getTime() - checkIn.getTime();
+		//Transforma os MILISEGUNDOS em DIAS para depois retornar.
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
+	
+}

c4936fdeb417950c46b5ee6edc600546bad00bff
afmnobre <lucas_guitar1987@hotmail.com>
2024-07-23 20:05:58 -0300

    Exercicio Reservation - Solução RUIM

--- Java/errosExcecoes3Personalizadas/src/application/Program.java
+++ Java/errosExcecoes3Personalizadas/src/application/Program.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import model.entities.Reservation;
+
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
@@ -19,6 +21,35 @@ public class Program {
 		System.out.print("Check-Out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date.");
+		} else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-Out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for updates must be future dates.");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservarion: Check-out date must be after check-in date.");
+			} else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
+			
+		}
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }

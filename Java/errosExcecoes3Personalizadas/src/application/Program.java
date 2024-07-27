f6c9decfad1f5fa1f23d5b89ea0d805415ef3f5f
afmnobre <lucas_guitar1987@hotmail.com>
2024-07-27 20:14:55 -0300

    Exercicio Xadres - POSITION CLASS

--- Java/errosExcecoes3Personalizadas/src/application/Program.java
+++ Java/errosExcecoes3Personalizadas/src/application/Program.java
@@ -43,7 +43,7 @@ public class Program {
 		catch (DomainException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
 		}
-		catch(RuntimeException e) {
+		catch (RuntimeException e) {
 			System.out.println("Unexpected error: " + e.getMessage());
 		}
 	
---
+++ Java/jogoxadres/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module jogoxadres {
+}

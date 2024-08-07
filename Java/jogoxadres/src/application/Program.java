7561f53c7ad56656d0e072466c7ad5ae39bfc085
afmnobre <lucas_guitar1987@hotmail.com>
2024-08-07 08:12:51 -0300

    Exercicio Xadres - Printing possible Moves

--- Java/jogoxadres/src/application/Program.java
+++ Java/jogoxadres/src/application/Program.java
@@ -25,10 +25,15 @@ public class Program {
 			try {
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces());
+				//source
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				//target
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
